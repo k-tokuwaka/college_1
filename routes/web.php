e3b7d62a910fc2e15f0a3b71535b04eff9907e16
k-tokuwaka <k.tokuwaka0902@gmail.com>
2023-12-29 02:33:44 +0900

    Add 初期設定＆基礎実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// /postsにGETリクエストが来たらPostControllerのindexメソッドを実行する。
+Route::get('/posts', [PostController::class, 'index']);
